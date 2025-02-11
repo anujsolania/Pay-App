b130d3868e7fad33cf05af39c68df4dfc771183a
anuj solania <anujsolania0507@gmail.com>
2025-02-11 23:54:40 +0530

    signIN page frontend

--- frontend/src/components/Signin.jsx
+++ frontend/src/components/Signin.jsx
@@ -1,6 +1,37 @@
-function Signin(params) {
+import { Link } from "react-router-dom";
+
+export function Signin() {
     return (
-        <>
-        </>
+        <div className="flex flex-col items-center justify-center h-screen w-screen" >
+            <div className="h-auto w-auto flex flex-col items-center gap-6 border rounded-lg" style={{padding: "2% 0"}} >
+                
+                <div className="flex flex-col items-center gap-1" >
+                    <h1 className="text-2xl font-bold " >Sign In</h1>
+                    <p className="text-center w-[90%]" >Enter your credentials to access your account</p>
+                </div>
+
+                <div className="flex flex-col gap-5 w-[90%]" >
+                <div >
+                    <h1 className="font-medium" >Email</h1>
+                    <input type="text" placeholder="harrypotter@gmail.com" className="border rounded w-full" style={{padding: "2%"}} ></input>
+                </div>
+                
+                <div>
+                    <h1 className="font-medium">Password</h1>
+                    <input type="text" className="border rounded w-full" style={{padding: "2%"}}  ></input>
+                </div>
+                </div>
+ 
+
+                <div className="flex flex-col items-center gap-3 w-full font-medium" style={{marginTop: "5%"}} >
+                <button className="rounded-md bg-black text-white w-[90%] " style={{padding: "5px"}} 
+                onClick={() => {
+
+                }} >Sign In</button>
+                <p className="font-medium" >Don't have an account? <Link to={"/signup"} className="underline">Sign Up</Link> </p>
+                </div>
+
+            </div>
+        </div>
     )
 }
--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,41 +1,45 @@
+import { Link } from "react-router-dom";
 
 export function Signup() {
     
     return (
-        <div className="flex flex-col items-center justify-center h-screen w-screen bg-gray-500" >
-        <div className="border h-[60%] w-[30%] flex flex-col items-center gap-6 rounded-lg " >
+        <div className="flex flex-col items-center justify-center h-screen w-screen" >
+        <div className="border h-auto w-auto flex flex-col items-center gap-6 rounded-lg " style={{padding: "2% 0"}} >
 
-            <div className="flex flex-col items-center gap-1" style={{marginTop: "7%"}} >
+            <div className="flex flex-col items-center gap-1">
             <h1 className="text-2xl font-bold " >Sign Up</h1>
             <p className="text-center w-[90%]" >Enter your information to create an account</p>
             </div>
 
-            <div className="flex flex-col items-center gap-5 w-[90%]" >
-                <div className="w-full" >
-                <h3>First Name</h3>
-                <input type="text" placeholder="Harry" className="border w-full"></input>
+            <div className="flex flex-col gap-5 w-[90%]" >
+                <div  >
+                <h3 className="font-medium" >First Name</h3>
+                <input type="text" placeholder="Harry" className="border rounded w-full" style={{padding: "2%"}} ></input>
                 </div>
 
-                <div className="w-full" >
-                <h3>Last Name</h3>
-                <input type="text" placeholder="Potter"className="border w-full" ></input>
+                <div  >
+                <h3 className="font-medium" >Last Name</h3>
+                <input type="text" placeholder="Potter"className="border rounded w-full" style={{padding: "2%"}} ></input>
                 </div>
 
-                <div className="w-full" >
-                <h3>Email</h3>
-                <input type="text" placeholder="harrypotter@gmail.com" className="border w-full" ></input>
+                <div >
+                <h3 className="font-medium" >Email</h3>
+                <input type="text" placeholder="harrypotter@gmail.com" className="border rounded w-full" style={{padding: "2%"}} ></input>
                 </div>
 
-                <div className="w-full" > 
-                <h3>Password</h3>
-                <input type="text"placeholder="lovehermione" className="border w-full" ></input>
+                <div > 
+                <h3 className="font-medium">Password</h3>
+                <input type="text"placeholder="lovehermione" className="border rounded w-full" style={{padding: "2%"}}></input>
                 </div>
              
             </div>
 
-            <div className="flex flex-col items-center gap-3" >
-            <button className="rounded-md bg-black text-white w-[100%] " style={{padding: "5px"}}  >Sign Up</button>
-            <p>Already have an account?<a href="/signin">Login</a></p>
+            <div className="flex flex-col items-center gap-3 w-full" style={{marginTop: "5%"}} >
+            <button className="rounded-md bg-black text-white w-[90%] " style={{padding: "5px"}} 
+            onClick={() => {
+
+            }} >Sign Up</button>
+            <p className="font-medium" >Already have an account? <Link to={"/signin"} className="underline" >Login</Link> </p>
             </div>
 
         </div>
