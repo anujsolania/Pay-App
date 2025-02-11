3b71a6f3e9a037e661fdc5c7ec9e896813ad3ce0
anuj solania <anujsolania0507@gmail.com>
2025-02-11 23:55:40 +0530

    dashboard page frontend

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,6 +1,14 @@
-function Dashboard(params) {
+import { Appbar } from "./smallcomponents/Appbar";
+import { Balance } from "./smallcomponents/Balance";
+import { Users } from "./smallcomponents/Users";
+
+export function Dashboard() {
     return (
-        <>
-        </>
+        <div>
+            <Appbar></Appbar>
+            <hr className="border-t border-gray-200" ></hr>
+            <Balance></Balance>
+            <Users></Users>
+        </div>
     )
 }
