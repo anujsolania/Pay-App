26f08225c41d833d261621b8a25c6b1e45f4a156
anuj solania <anujsolania0507@gmail.com>
2025-02-12 01:32:51 +0530

    frontend done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { Signup } from './components/Signup';
+import { Signin } from './components/Signin';
+import { Dashboard } from './components/Dashboard';
+import { Sendmoney } from './components/Sendmoney';
 
 function App() {
 
@@ -10,9 +13,9 @@ function App() {
     <Router>
     <Routes>
       <Route path="/" element={<Signup></Signup>} ></Route>
-      {/* <Route path="/signin" element={<Signin></Signin>} ></Route>
-      <Route path="/send" element={<Sendmoney></Sendmoney>} ></Route>
-      <Route path="/dashboard" element={<Dashboard></Dashboard>} ></Route> */}
+      <Route path="/signin" element={<Signin></Signin>} ></Route>
+      <Route path="/sendmoney" element={<Sendmoney></Sendmoney>} ></Route>
+      <Route path="/dashboard" element={<Dashboard></Dashboard>} ></Route>
     </Routes>
     </Router>
     </>
